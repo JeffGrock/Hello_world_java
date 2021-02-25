8c4089d6b3c1bceaa65c093a652056b693a8c02e
jeffgrock <jeff.grockowski@protonmail.com>
2021-02-25 12:10:14 -0500

    first commit

---
+++ JeffGrockowskiProject01.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class JeffGrockowskiProject01 here.
+ *
+ * @author Jeff Grockowski
+ * @version 08/22/2019
+ * 
+ * Question 6: After compiling, i'm notified that there is a compiler error that
+ *              "Package system does not exist".
+ * Question 7: Breaking the line of code after "Hello" causes a whole host of
+ *               syntax errors on both lines. These are pointed out to me my the compiler.
+ * Question 8: The program compiles and runs without error, however, the
+ *              second sentence is printed on the same line as the first.
+ *               
+ */
+public class JeffGrockowskiProject01
+{
+   public static void main (String [] args) {
+       System.out.println("Hello, World!");
+       System.out.println("This is my first Java Program");
+    }
+}
